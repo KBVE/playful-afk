c9373d094ea87e8007d6737fd0f4a796f989c4d1
Richie911 <oohrichmond.k@gmail.com>
2025-10-20 05:50:17 +0000

    Deploying to gh-pages from @ KBVE/playful-afk@629cd98129d24d2c76f2e3a3a377b07b2c1da68a 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1760939199|5089606';
+const CACHE_VERSION = '1760939405|5057572';
 /** @type {string} */
 const CACHE_PREFIX = 'afk-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
