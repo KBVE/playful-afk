90c504f288fca92b16fc30aa88063648ef87949b
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-10-22 23:49:41 +0000

    Deploying to gh-pages from @ KBVE/playful-afk@8b11008603c3a2b58ce5506464d9c422302b8c5e 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1761174737|7158403';
+const CACHE_VERSION = '1761176969|7189669';
 /** @type {string} */
 const CACHE_PREFIX = 'afk-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
